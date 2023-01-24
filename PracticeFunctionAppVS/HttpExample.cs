437ba8350fead43151f3d95268ee9931c29e6cdf
= <kinkeadian@outlook.com>
2023-01-24 11:38:10 -0800

    adding in queue

--- PracticeFunctionAppVS/HttpExample.cs
+++ PracticeFunctionAppVS/HttpExample.cs
@@ -15,6 +15,7 @@ namespace PracticeFunctionAppVS
         [FunctionName("HttpExample")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            [Queue("outqueue"), StorageAccount("AzureWebJobsStorage")] ICollector<string> msg,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
@@ -25,11 +26,14 @@ namespace PracticeFunctionAppVS
             dynamic data = JsonConvert.DeserializeObject(requestBody);
             name = name ?? data?.name;
 
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
-            return new OkObjectResult(responseMessage);
+            if (!string.IsNullOrEmpty(name))
+            {
+                // Add a message to the output collection.
+                msg.Add(name);
+            }
+            return name != null
+                ? (ActionResult)new OkObjectResult($"Hello, {name}")
+                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
         }
     }
 }
